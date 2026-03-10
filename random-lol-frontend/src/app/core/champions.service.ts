30ff8cb6fe3489f7f0e668882163a640677e5f9f
Moa <lenyantunes13@gmail.com>
2026-03-10 14:01:28 +0100

    ajout du check pour le prerendering

--- random-lol-frontend/src/app/core/champions.service.ts
+++ random-lol-frontend/src/app/core/champions.service.ts
@@ -1,16 +1,21 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, PLATFORM_ID, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { catchError, map, Observable, throwError } from 'rxjs';
 import { Champion, ChampionsByRole } from './models/champion.model';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChampionsService {
   private http = inject(HttpClient);
+  private platformId = inject(PLATFORM_ID); // check
 
   getChampions(): Observable<any> {
-    return this.http.get<any>('/api/champions');
+    const apiUrl = isPlatformServer(this.platformId)
+      ? 'http://localhost:3000/api/champions' // pour le prerendering, utilisation de l'url local
+      : '/api/champions'; // url relative pour prod
+    return this.http.get<any>(apiUrl);
   }
 
   getChampionsByRole(role: string): Observable<ChampionsByRole> {
